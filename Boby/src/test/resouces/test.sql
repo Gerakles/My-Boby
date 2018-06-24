5b03dc94a76598140aefe08274605890a731b328
Gerakles <bogdania567@gmail.com>
2018-06-24 23:57:08 +0300

    Yakim 457

---
+++ Boby/src/test/resouces/test.sql
@@ -0,0 +1,6 @@
+insert into users (name, age, email) values ('Yakim', 19, 'bogdania567@gmail.com');
+#select * from users;
+#SELECT name, age FROM users where id = 1;
+#update users set name='Steve', age=55 where id=5;
+#delete from users;
+#delete from users where id=3;
