cd818ab5272482645501b34ea32c7f273d2b76ec
Gerakles <bogdania567@gmail.com>
2018-07-02 23:02:36 +0300

    Yakim 451

--- Boby/src/main/java/Rps.java
+++ Boby/src/main/java/Rps.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Rps {
     /*
@@ -8,106 +8,79 @@ public class Rps {
         r, p, s
     */
     public static void main(String[] args) {
-
         try {
-            Scanner sc =
-                    new Scanner(System.in);
-
+            Scanner sc = new Scanner( System.in );
             if (sc.hasNext()) { //Checks for null values
-
                 String userInput = quickFormat( sc.next() );
                 sc.close();
-
                 if (isValid( userInput )) {
                     game( userInput );
-
                 } else {
                     displayInputs();
                 }
             } else {
                 displayInputs(); //Null value, displaying correct inputs
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
     public static void game(String user) {
         String computer = computerResults();
-
-        print( user + " vs " + computer + "\n");
-
-        if (user.equalsIgnoreCase(computer)) {
-            print("Stalemate! No winners.");
+        print( user + " vs " + computer + "\n" );
+        if (user.equalsIgnoreCase( computer )) {
+            print( "Stalemate! No winners." );
         } else {
-
-            if (checkWin(user, computer)) {
-                print("You won against the computer!");
+            if (checkWin( user, computer )) {
+                print( "You won against the computer!" );
             } else {
-                print("You lost against the computer!");
+                print( "You lost against the computer!" );
             }
         }
     }
 
     public static String computerResults() {
-
-        String types[] =
-                {"rock", "paper", "scissors"};
-
+        String types[] = {"rock", "paper", "scissors"};
         Random rand = new Random();
-        int computerChoice = rand.nextInt(3);
-
+        int computerChoice = rand.nextInt( 3 );
         return types[computerChoice];
     }
 
     public static boolean isValid(String input) {
-
-
-        switch( input.toLowerCase()) {
-
+        switch (input.toLowerCase()) {
             case "rock":
                 return true;
-
             case "paper":
                 return true;
-
             case "scissors":
                 return true;
-
             default:
                 return false;
         }
     }
 
     public static boolean checkWin(String user, String opponent) {
-
-        if ( (!isValid( user )) || (!isValid( opponent )) ) {
+        if ((!isValid( user )) || (!isValid( opponent ))) {
             return false;
         }
-
         String rock = "rock", paper = "paper", scissors = "scissors";
-
-        if ( (user.equalsIgnoreCase( rock )) && (opponent.equalsIgnoreCase( scissors )) ) {
+        if ((user.equalsIgnoreCase( rock )) && (opponent.equalsIgnoreCase( scissors ))) {
             return true;
         }
-
-        if ( (user.equalsIgnoreCase( scissors)) && (opponent.equalsIgnoreCase( paper )) ) {
+        if ((user.equalsIgnoreCase( scissors )) && (opponent.equalsIgnoreCase( paper ))) {
             return true;
         }
-
-        if ( (user.equalsIgnoreCase( paper )) && (opponent.equalsIgnoreCase( rock  )) ) {
+        if ((user.equalsIgnoreCase( paper )) && (opponent.equalsIgnoreCase( rock ))) {
             return true;
         }
-
         return false;
         //If no possible win, assume loss.
     }
 
     public static void displayInputs() {
         //One place to edit it all!
-        print("Invalid user input!\nWrite rock, paper or scissors!");
+        print( "Invalid user input!\nWrite rock, paper or scissors!" );
     }
 
     public static void print(String text) {
@@ -117,28 +90,18 @@ public class Rps {
 
     public static String quickFormat(String input) {
         //Just some quick function to shorten inputs.
-
         String output = input;
-
-        switch( input.toLowerCase() ) {
-
+        switch (input.toLowerCase()) {
             case "r":
                 output = "rock";
                 break;
-
             case "p":
                 output = "paper";
                 break;
-
             case "s":
                 output = "scissors";
                 break;
-
-            case "scissor":
-                output = "scissors";
-                break;
         }
-
         return output;
     }
 }
