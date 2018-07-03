8f13e8a61c011fc90581ccad8ad14e82c4d45383
Gerakles <bogdania567@gmail.com>
2018-07-03 15:20:43 +0300

    Yakim 450

--- Boby/src/main/java/BackgroundSample.java
+++ Boby/src/main/java/BackgroundSample.java
@@ -1,32 +1,28 @@
+import javax.swing.*;
 import java.awt.*;
-
-import javax.swing.GrayFilter;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 public class BackgroundSample {
     public static void main(String args[]) {
-        JFrame frame = new JFrame("Background Example");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        final ImageIcon imageIcon = new ImageIcon("3^7.png");
+        JFrame frame = new JFrame( "Background Example" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        final ImageIcon imageIcon = new ImageIcon( "3^7.png" );
         JTextArea output = new JTextArea() {
             Image image = imageIcon.getImage();
+
             {
-                setOpaque(false);
+                setOpaque( false );
             }
 
             public void paint(Graphics g) {
-                g.drawImage(image, 0, 0, this);
-                super.paint(g);
+                g.drawImage( image, 0, 0, this );
+                super.paint( g );
             }
         };
         output.setBackground( Color.gray );
-        JScrollPane scrollPane = new JScrollPane(output);
+        JScrollPane scrollPane = new JScrollPane( output );
         Container content = frame.getContentPane();
-        content.add(scrollPane, BorderLayout.CENTER);
-        frame.setSize(350, 300);
-        frame.setVisible(true);
+        content.add( scrollPane, BorderLayout.CENTER );
+        frame.setSize( 350, 300 );
+        frame.setVisible( true );
     }
 }
