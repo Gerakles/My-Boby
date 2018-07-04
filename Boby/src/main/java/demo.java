fb41bfc84ae3846d64923d91cdfe8dd73be06c80
Gerakles <bogdania567@gmail.com>
2018-07-04 17:13:00 +0300

    Yakim 449

--- Boby/src/main/java/demo.java
+++ Boby/src/main/java/demo.java
@@ -1,14 +1,7 @@
 public class demo {
     public static void main(String[] args){
-        String temp = "HELLO! my Lord";
-
-        for (char i : temp.toCharArray()) {
-            System.out.print( String.valueOf( i ) );
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e1) {
-                e1.printStackTrace();
-            }
+        for (int i = 0; i < 10; i++) {
+            System.out.println((Integer)i);
         }
     }
 }
