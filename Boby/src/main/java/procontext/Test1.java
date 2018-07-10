380a6735ba7143caf7a7584e4877e5126493d3fe
Gerakles <bogdania567@gmail.com>
2018-07-10 18:05:06 +0300

    Yakim 443

---
+++ Boby/src/main/java/procontext/Test1.java
@@ -0,0 +1,16 @@
+package procontext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Person {
+    private String name;
+    private Integer age;
+    private List<String> geoHistory = new ArrayList <>(  );
+}
