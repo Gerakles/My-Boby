4b8aa93e0b6e2d8e0f51d782b93144ed4324a1b6
Gerakles <bogdania567@gmail.com>
2018-07-10 18:37:37 +0300

    Yakim 443

--- Boby/src/main/java/procontext/Test1.java
+++ Boby/src/main/java/procontext/Test1.java
@@ -1,16 +1,31 @@
 package procontext;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Test1 {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     public static void main(String[] args) {
-
+        Person person = new Person("Bob", -443, Arrays.asList("Odessa", "Bucovel", "Hmelinitsck"));
+        String json = GSON.toJson( person );
+        System.out.println(json);
     }
 }
 
 class Person {
     private String name;
     private Integer age;
-    private List<String> geoHistory = new ArrayList <>(  );
+    @SerializedName( "goe" )
+    private List<String> geoHistory;
+
+   Person(String name, Integer age, List <String> geoHistory) {
+        this.name = name;
+        this.age = age;
+        this.geoHistory = geoHistory;
+    }
 }
