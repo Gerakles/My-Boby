970db314b054ef7979dfe25cc522762415a58f0e
Gerakles <bogdania567@gmail.com>
2018-07-11 04:59:45 +0300

    Yakim 442

--- Boby/src/main/java/procontext/Test.java
+++ Boby/src/main/java/procontext/Test.java
@@ -2,10 +2,17 @@ package procontext;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class Test {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
         DataInputStream dis = new DataInputStream( new FileInputStream( "res\\file.txt" ) );
+
+        byte[] buffer = new byte[512];
+        while (dis.available() != 0) {
+            int count = dis.read( buffer );
+
+            if (count>0)
+                System.out.println(new String( buffer ));
+        }
     }
 }
