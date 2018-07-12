dd0e363d7e19636b9cbd9c1a39b9bca1779fff34
Gerakles <bogdania567@gmail.com>
2018-07-12 18:01:18 +0300

    Yakim 441

--- Boby/src/main/java/procontext/ExampleOfAnHTTPJavaRequest/Test.java
+++ Boby/src/main/java/procontext/ExampleOfAnHTTPJavaRequest/Test.java
@@ -1,4 +1,16 @@
 package procontext.ExampleOfAnHTTPJavaRequest;
 
+import java.net.HttpURLConnection;
+
 public class Test {
+    public static void main(String[] args) {
+        String query = "https://vk.com/im?peers=140620469&sel=-157826406";
+
+        HttpURLConnection connection = null;
+        try {
+
+        } catch (Throwable cause) {
+            cause.printStackTrace();
+        }
+    }
 }
