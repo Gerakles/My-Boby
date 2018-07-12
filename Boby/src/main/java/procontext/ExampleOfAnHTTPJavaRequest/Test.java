2aa4feb05d5d114c0e0ccc49948634ef9ccb53d5
Gerakles <bogdania567@gmail.com>
2018-07-12 18:35:46 +0300

    Yakim 441

--- Boby/src/main/java/procontext/ExampleOfAnHTTPJavaRequest/Test.java
+++ Boby/src/main/java/procontext/ExampleOfAnHTTPJavaRequest/Test.java
@@ -1,6 +1,9 @@
 package procontext.ExampleOfAnHTTPJavaRequest;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Test {
     public static void main(String[] args) {
@@ -8,9 +11,31 @@ public class Test {
 
         HttpURLConnection connection = null;
         try {
+            connection = (HttpURLConnection) new URL(query).openConnection();
+            connection.setUseCaches( false );
+            connection.setConnectTimeout( 250 );
+            connection.setReadTimeout( 250 );
 
+            connection.connect();
+
+            StringBuilder sb = new StringBuilder(  );
+
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
+                BufferedReader in = new BufferedReader( new InputStreamReader( connection.getInputStream(), "cp1251" ) );
+
+                String line;
+                while ((line=in.readLine()) != null) {
+                    sb.append( line );
+                    sb.append( "\n" );
+                }
+                System.out.println(sb.toString(  ));
+            } else
+                System.out.println("fail: "+connection.getResponseCode()+", "+connection.getResponseMessage());
         } catch (Throwable cause) {
             cause.printStackTrace();
+        } finally {
+            if (connection != null)
+                connection.disconnect();
         }
     }
 }
