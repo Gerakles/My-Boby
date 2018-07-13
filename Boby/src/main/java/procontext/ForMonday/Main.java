da04245e1a62322d34be3e9807e1574a2fae5f95
Gerakles <bogdania567@gmail.com>
2018-07-13 14:18:28 +0300

    Yakim 440

--- Boby/src/main/java/procontext/ForMonday/Main.java
+++ Boby/src/main/java/procontext/ForMonday/Main.java
@@ -1,4 +1,26 @@
 package procontext.ForMonday;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
+    public static void main(String[] args) {
+        String query = "https://vk.com/im?peers=140620469&sel=-157826406";
+
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) new URL( query ).openConnection();
+            connection.connect();
+
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
+                System.out.println("ok");
+            } else
+                System.out.println( "fail: " + connection.getResponseCode() + ", " + connection.getResponseMessage() );
+        } catch (Throwable cause) {
+            cause.printStackTrace();
+        } finally {
+            if (connection != null)
+                connection.disconnect();
+        }
+    }
 }
