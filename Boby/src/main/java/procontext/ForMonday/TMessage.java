4aef521499d458e594a4a1b4c1dc2b5304302288
Gerakles <bogdania567@gmail.com>
2018-07-15 23:01:28 +0300

    Yakim 438

--- Boby/src/main/java/procontext/ForMonday/TMessage.java
+++ Boby/src/main/java/procontext/ForMonday/TMessage.java
@@ -1,5 +1,9 @@
 package procontext.ForMonday;
 
 public class TMessage {
+    private static final String HOST = "jdbc:mysql://localhost:3306/mydbtest?useSSL=false&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
+    private static final String INSERT_NEW = "INSERT INTO brain VALUES(?,?,?)";
 }
--- Boby/src/main/java/procontext/ForMonday/TStatus.java
+++ Boby/src/main/java/procontext/ForMonday/TStatus.java
@@ -1,4 +1,9 @@
 package procontext.ForMonday;
 
 public class TStatus {
+    private static final String HOST = "jdbc:mysql://localhost:3306/mydbtest?useSSL=false&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private static final String INSERT_NEW = "INSERT INTO brain VALUES(?,?,?)";
 }
