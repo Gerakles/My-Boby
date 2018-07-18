73ec386d0bc7308dddb9c7a2334fa637d43666fd
Gerakles <bogdania567@gmail.com>
2018-07-18 23:17:31 +0300

    Yakim 435

--- Boby/src/main/java/procontext/ForMonday/Main.java
+++ Boby/src/main/java/procontext/ForMonday/Main.java
@@ -2,7 +2,7 @@ package procontext.ForMonday;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main { // 398 b
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in );
         while (sc.hasNext()) {
