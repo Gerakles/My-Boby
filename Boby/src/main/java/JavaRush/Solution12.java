ded15a4d3f1cf3a2753af74bb5cf996879e360bc
Gerakles <bogdania567@gmail.com>
2018-07-29 23:01:29 +0300

    Yakim 426                                                       '+' 55

---
+++ Boby/src/main/java/JavaRush/Solution12.java
@@ -0,0 +1,15 @@
+package JavaRush;
+
+/*
+выводим квадрат числа
+ */
+
+import java.util.Scanner;
+
+public class Solution12 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner( System.in );
+        int y = s.nextInt();
+        System.out.println( y * y );
+    }
+}
