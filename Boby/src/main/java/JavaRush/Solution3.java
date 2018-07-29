5511aa3012391aff5bd149de18d128d9396c6e8b
Gerakles <bogdania567@gmail.com>
2018-07-29 17:00:08 +0300

    Yakim 425                                                      '+' 54

---
+++ Boby/src/main/java/JavaRush/Solution3.java
@@ -0,0 +1,11 @@
+package JavaRush;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+    }
+}
