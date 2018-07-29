385de4b9ce96238117ca1bd5a61b6a3d63a3052a
Gerakles <bogdania567@gmail.com>
2018-07-29 16:55:32 +0300

    Yakim 425                                                      '+' 54

---
+++ Boby/src/main/java/JavaRush/Solution1.java
@@ -0,0 +1,11 @@
+package JavaRush;
+
+/*
+Амиго очень умный
+*/
+
+public class Solution1 {
+    public static void main(String[] args) {
+        System.out.println("Амиго очень умный");
+    }
+}
---
+++ Boby/src/main/java/JavaRush/Solution2.java
@@ -0,0 +1,11 @@
+package JavaRush;
+
+/*
+Круто быть программистом!
+*/
+
+public class Solution2 {
+    public static void main(String[] args) {
+        System.out.println("Я думаю, быть программистом - это круто");
+    }
+}
