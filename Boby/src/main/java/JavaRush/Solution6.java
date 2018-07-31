a1184142238ec25b85545ea9a102e3a8f3e29f96
Gerakles <bogdania567@gmail.com>
2018-07-31 21:28:21 +0300

    Yakim 424                                                     '+' 53

--- Boby/src/main/java/JavaRush/Solution6.java
+++ Boby/src/main/java/JavaRush/Solution6.java
@@ -6,6 +6,6 @@ package JavaRush;
 
 public class Solution6 {
     public static void main(String[] args) {
-        System.out.println("Это не баг, это фича.");
+        System.out.println( "Это не баг, это фича." );
     }
 }
