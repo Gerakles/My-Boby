b5692c7984570cebb56b81750078fbd7ce2a21c1
Gerakles <bogdania567@gmail.com>
2018-07-31 21:24:13 +0300

    Yakim 424                                                     '+' 53

--- Boby/src/main/java/JavaRush/Matrice.java
+++ Boby/src/main/java/JavaRush/Matrice.java
@@ -2,16 +2,17 @@ package JavaRush;
 
 public class Matrice {
     public static void main(String[] args) {
-        int Nx=9,Ny=6; int[][] m=new int[Nx][Ny];
+        int Nx = 9, Ny = 6;
+        int[][] m = new int[Nx][Ny];
 
-        for(int d=1,r=1,x=-1,y=0,b=Nx;1<=(d%2==0?Nx--:Ny--);b+=d++%2==0?Nx:Ny)
-            while (r<=b) m[x-=(d%4-2)%2][y+=(d%4-1)%2]=r++;
+        for (int d = 1, r = 1, x = -1, y = 0, b = Nx; 1 <= (d % 2 == 0 ? Nx-- : Ny--); b += d++ % 2 == 0 ? Nx : Ny)
+            while (r <= b) m[x -= (d % 4 - 2) % 2][y += (d % 4 - 1) % 2] = r++;
 
 //проверка
-        for (int i = 0; i <9 ; i++) {
+        for (int i = 0; i < 9; i++) {
             System.out.println();
-            for (int j = 0; j <6 ; j++) {
-                System.out.print(m[i][j]+" ");
+            for (int j = 0; j < 6; j++) {
+                System.out.print( m[i][j] + " " );
             }
         }
     }
