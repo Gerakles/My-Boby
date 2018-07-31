97f9ca29411b50d0917fed59e506a19ed4faaabc
Gerakles <bogdania567@gmail.com>
2018-07-31 16:13:53 +0300

    Yakim 424                                                     '+' 53

---
+++ Boby/src/main/java/JavaRush/Matematical.java
@@ -0,0 +1,10 @@
+package JavaRush;
+
+public class Matematical {
+    public static void main(String[] args) {
+        double a = 5.5;
+        int b = (int) a;
+        double c = a - b;
+        System.out.println( c );
+    }
+}
