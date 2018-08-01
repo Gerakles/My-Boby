aed2964a5bb2738f26fa1e9e04c529cf62328c03
Gerakles <bogdania567@gmail.com>
2018-08-01 23:00:51 +0300

    Yakim 424                                                     '+' 53

--- Boby/src/main/java/JavaRush/Solution10.java
+++ Boby/src/main/java/JavaRush/Solution10.java
@@ -7,6 +7,6 @@ package JavaRush;
 public class Solution10 {
     public static void main(String[] args) {
         String name = "It's magic and it works";
-        System.out.println(name);
+        System.out.println( name );
     }
 }
