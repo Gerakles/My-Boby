da7b3ab32f4db720f2c69497b04153ffd2156f56
Gerakles <bogdania567@gmail.com>
2018-08-04 08:37:22 +0300

    Yakim 413                                                 '+' 49

--- Boby/src/main/java/JavaRush/DecoratorTest.java
+++ Boby/src/main/java/JavaRush/DecoratorTest.java
@@ -3,42 +3,51 @@ package JavaRush;
 interface Carssss {
     void draw();
 }
+
 class SportCarss implements Carssss {
     public void draw() {
-        System.out.println("SportCar");
+        System.out.println( "SportCar" );
     }
 }
+
 class UnknownCarss implements Carssss {
     public void draw() {
-        System.out.println("UnknownCar");
+        System.out.println( "UnknownCar" );
     }
 }
+
 abstract class CarDecorator implements Carssss {
     protected Carssss decorated;
-    public CarDecorator(Carssss decorated){
+
+    public CarDecorator(Carssss decorated) {
         this.decorated = decorated;
     }
-    public void draw(){
+
+    public void draw() {
         decorated.draw();
     }
 }
+
 class BlueCarDecorator extends CarDecorator {
     public BlueCarDecorator(Carssss decorated) {
-        super(decorated);
+        super( decorated );
     }
+
     public void draw() {
         decorated.draw();
         setColor();
     }
-    private void setColor(){
-        System.out.println("Color: red");
+
+    private void setColor() {
+        System.out.println( "Color: red" );
     }
 }
 
 public class DecoratorTest {//тест
+
     public static void main(String[] args) {
         Carssss sportCar = new SportCarss();
-        Carssss blueUnknownCar = new BlueCarDecorator(new UnknownCarss());
+        Carssss blueUnknownCar = new BlueCarDecorator( new UnknownCarss() );
         sportCar.draw();
         System.out.println();
         blueUnknownCar.draw();
