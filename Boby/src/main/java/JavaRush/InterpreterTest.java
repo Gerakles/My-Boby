29ce31779b31a94873bce00afff16ee9941eb6ef
Gerakles <bogdania567@gmail.com>
2018-08-04 08:33:02 +0300

    Yakim 413                                                 '+' 49

--- Boby/src/main/java/JavaRush/InterpreterTest.java
+++ Boby/src/main/java/JavaRush/InterpreterTest.java
@@ -3,42 +3,51 @@ package JavaRush;
 interface Expression {
     String interpret(Contexts context);
 }
+
 class Contexts {
-    public String getLoverCase(String s){
+    public String getLoverCase(String s) {
         return s.toLowerCase();
     }
-    public String getUpperCase(String s){
+
+    public String getUpperCase(String s) {
         return s.toUpperCase();
     }
 }
+
 class LoverExpression implements Expression {
     private String s;
+
     public LoverExpression(String s) {
         this.s = s;
     }
+
     public String interpret(Contexts context) {
-        return context.getLoverCase(s);
+        return context.getLoverCase( s );
     }
 }
+
 class UpperExpression implements Expression {
     private String s;
+
     public UpperExpression(String s) {
         this.s = s;
     }
+
     public String interpret(Contexts context) {
-        return context.getUpperCase(s);
+        return context.getUpperCase( s );
     }
 }
 
 public class InterpreterTest {//тест
+
     public static void main(String[] args) {
         String str = "TesT";
         Contexts context = new Contexts();
-        Expression loverExpression = new LoverExpression(str);
-        str = loverExpression.interpret(context);
-        System.out.println(str);
-        Expression upperExpression = new UpperExpression(str);
-        str = upperExpression.interpret(context);
-        System.out.println(str);
+        Expression loverExpression = new LoverExpression( str );
+        str = loverExpression.interpret( context );
+        System.out.println( str );
+        Expression upperExpression = new UpperExpression( str );
+        str = upperExpression.interpret( context );
+        System.out.println( str );
     }
 }
