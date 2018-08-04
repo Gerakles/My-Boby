0110448b41cc626571243199deefba781805ae16
Gerakles <bogdania567@gmail.com>
2018-08-04 08:34:42 +0300

    Yakim 413                                                 '+' 49

--- Boby/src/main/java/JavaRush/FacadeTest.java
+++ Boby/src/main/java/JavaRush/FacadeTest.java
@@ -2,35 +2,44 @@ package JavaRush;
 
 interface Carss {
     void start();
+
     void stop();
 }
+
 class Key implements Carss {
     public void start() {
-        System.out.println("Вставить ключи");
+        System.out.println( "Вставить ключи" );
     }
+
     public void stop() {
-        System.out.println("Вытянуть ключи");
+        System.out.println( "Вытянуть ключи" );
     }
 }
+
 class Engines implements Carss {
     public void start() {
-        System.out.println("Запустить двигатель");
+        System.out.println( "Запустить двигатель" );
     }
+
     public void stop() {
-        System.out.println("Остановить двигатель");
+        System.out.println( "Остановить двигатель" );
     }
 }
+
 class Facade {
     private Key key;
     private Engines engine;
+
     public Facade() {
         key = new Key();
         engine = new Engines();
     }
+
     public void startCar() {
         key.start();
         engine.start();
     }
+
     public void stoptCar() {
         key.stop();
         engine.stop();
@@ -38,6 +47,7 @@ class Facade {
 }
 
 public class FacadeTest {//тест
+
     public static void main(String[] args) {
         Facade facade = new Facade();
         facade.startCar();
