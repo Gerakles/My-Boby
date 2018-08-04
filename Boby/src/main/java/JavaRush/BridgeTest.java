d6e9cca1ef35b70fc091bfe967864fee8546ac4a
Gerakles <bogdania567@gmail.com>
2018-08-04 08:30:10 +0300

    Yakim 413                                                 '+' 49

--- Boby/src/main/java/JavaRush/BridgeTest.java
+++ Boby/src/main/java/JavaRush/BridgeTest.java
@@ -3,47 +3,58 @@ package JavaRush;
 interface Enginess {
     void setEngine();
 }
+
 abstract class Carsss {
     protected Enginess engine;
-    public Carsss(Enginess engine){
+
+    public Carsss(Enginess engine) {
         this.engine = engine;
     }
+
     abstract public void setEngine();
 }
+
 class SportCars extends Carsss {
     public SportCars(Enginess engine) {
-        super(engine);
+        super( engine );
     }
+
     public void setEngine() {
-        System.out.print("SportCar engine: ");
+        System.out.print( "SportCar engine: " );
         engine.setEngine();
     }
 }
+
 class UnknownCars extends Carsss {
     public UnknownCars(Enginess engine) {
-        super(engine);
+        super( engine );
     }
+
     public void setEngine() {
-        System.out.print("UnknownCar engine: ");
+        System.out.print( "UnknownCar engine: " );
         engine.setEngine();
     }
 }
+
 class SportEngine implements Enginess {
-    public void setEngine(){
-        System.out.println("sport");
+    public void setEngine() {
+        System.out.println( "sport" );
     }
 }
+
 class UnknownEngine implements Enginess {
-    public void setEngine(){
-        System.out.println("unknown");
+    public void setEngine() {
+        System.out.println( "unknown" );
     }
 }
+
 public class BridgeTest {//тест
+
     public static void main(String[] args) {
-        Carsss sportCar = new SportCars(new SportEngine());
+        Carsss sportCar = new SportCars( new SportEngine() );
         sportCar.setEngine();
         System.out.println();
-        Carsss unknownCar = new UnknownCars(new UnknownEngine());
+        Carsss unknownCar = new UnknownCars( new UnknownEngine() );
         unknownCar.setEngine();
     }
 }
