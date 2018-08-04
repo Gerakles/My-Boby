20f0e9af2b70e182b4d884e94b1e11de9ddcb75e
Gerakles <bogdania567@gmail.com>
2018-08-04 08:39:27 +0300

    Yakim 413                                                 '+' 49

--- Boby/src/main/java/JavaRush/ChainofResponsibilityTest.java
+++ Boby/src/main/java/JavaRush/ChainofResponsibilityTest.java
@@ -2,32 +2,40 @@ package JavaRush;
 
 interface Payment {
     void setNext(Payment payment);
+
     void pay();
 }
+
 class VisaPayment implements Payment {
     private Payment payment;
+
     public void setNext(Payment payment) {
         this.payment = payment;
     }
+
     public void pay() {
-        System.out.println("Visa Payament");
+        System.out.println( "Visa Payament" );
     }
 }
+
 class PayPalPayment implements Payment {
     private Payment payment;
+
     public void setNext(Payment payment) {
         this.payment = payment;
     }
+
     public void pay() {
-        System.out.println("PayPal Payament");
+        System.out.println( "PayPal Payament" );
     }
 }
 
 public class ChainofResponsibilityTest {//тест
+
     public static void main(String[] args) {
         Payment visaPayment = new VisaPayment();
         Payment payPalPayment = new PayPalPayment();
-        visaPayment.setNext(payPalPayment);
+        visaPayment.setNext( payPalPayment );
         visaPayment.pay();
     }
 }
