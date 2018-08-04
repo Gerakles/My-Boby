3a93882bf5cc536b4f17ba5d1c1052388914df11
Gerakles <bogdania567@gmail.com>
2018-08-04 08:38:28 +0300

    Yakim 413                                                 '+' 49

--- Boby/src/main/java/JavaRush/CommandTest.java
+++ Boby/src/main/java/JavaRush/CommandTest.java
@@ -3,48 +3,60 @@ package JavaRush;
 interface Command {
     void execute();
 }
+
 class Carssssss {
     public void startEngine() {
-        System.out.println("запустить двигатель");
+        System.out.println( "запустить двигатель" );
     }
+
     public void stopEngine() {
-        System.out.println("остановить двигатель");
+        System.out.println( "остановить двигатель" );
     }
 }
+
 class StartCar implements Command {
     Carssssss car;
+
     public StartCar(Carssssss car) {
         this.car = car;
     }
+
     public void execute() {
         car.startEngine();
     }
 }
+
 class StopCar implements Command {
     Carssssss car;
+
     public StopCar(Carssssss car) {
         this.car = car;
     }
+
     public void execute() {
         car.stopEngine();
     }
 }
+
 class CarInvoker {
     public Command command;
-    public CarInvoker(Command command){
+
+    public CarInvoker(Command command) {
         this.command = command;
     }
-    public void execute(){
+
+    public void execute() {
         this.command.execute();
     }
 }
 
 public class CommandTest {//тест
+
     public static void main(String[] args) {
         Carssssss car = new Carssssss();
-        StartCar startCar = new StartCar(car);
-        StopCar stopCar = new StopCar(car);
-        CarInvoker carInvoker = new CarInvoker(startCar);
+        StartCar startCar = new StartCar( car );
+        StopCar stopCar = new StopCar( car );
+        CarInvoker carInvoker = new CarInvoker( startCar );
         carInvoker.execute();
     }
 }
