f78a80f150207ae2bab020494a8a63af60561859
Gerakles <bogdania567@gmail.com>
2018-08-07 13:11:28 +0300

    Yakim 411                                               '+' 47

--- Boby/src/main/java/JavaRush/Hibernate/utils/HibernateSessionFactoryUtil.java
+++ Boby/src/main/java/JavaRush/Hibernate/utils/HibernateSessionFactoryUtil.java
@@ -4,9 +4,7 @@ import JavaRush.Hibernate.models.Auto;
 import JavaRush.Hibernate.models.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration
-
-import javax.security.auth.login.Configuration;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateSessionFactoryUtil {
     private static SessionFactory sessionFactory;
