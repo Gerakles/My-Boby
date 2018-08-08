8e23dfaef687a1c23442848a5dd5fcb236708af7
Gerakles <bogdania567@gmail.com>
2018-08-08 21:09:03 +0300

    Yakim 410                                              '+' 46

--- Boby/src/main/java/JavaRush/Hibernate/models/Auto.java
+++ Boby/src/main/java/JavaRush/Hibernate/models/Auto.java
@@ -27,31 +27,24 @@ public class Auto {
     public int getId() {
         return id;
     }
-
     public String getModel() {
         return model;
     }
-
     public void setModel(String model) {
         this.model = model;
     }
-
     public String getColor() {
         return color;
     }
-
     public void setColor(String color) {
         this.color = color;
     }
-
     public User getUser() {
         return user;
     }
-
     public void setUser(User user) {
         this.user = user;
     }
-
     @Override
     public String toString() {
         return color + " " + model;
