c5cd99042126db49911988b950a351e38d1ad982
Gerakles <bogdania567@gmail.com>
2018-08-13 21:49:50 +0300

    Yakim 405                                         '+' 41

--- Boby/src/main/java/JavaRush/example/User.java
+++ Boby/src/main/java/JavaRush/example/User.java
@@ -1,15 +1,31 @@
 package JavaRush.example;
 
 import java.util.Map;
+import java.util.Objects;
 
-class Sex{}
+class Sex {
+}
 
 public class User {
+    private static Map <Integer, User> allUsers;
+    private static int countId = 0;
     private int id;
     private String name;
     private int age;
     private Sex sex;
 
-    private static Map<Integer, User> allUsers;
-    private static int countId = 0;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals( name, user.name ) &&
+                sex == user.sex;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( name, age, sex );
+    }
 }
