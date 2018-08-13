c210f2c88ebcc49a4b505c4e7af83dd6d2a3a649
Gerakles <bogdania567@gmail.com>
2018-08-13 21:46:19 +0300

    Yakim 405                                         '+' 41

--- Boby/src/main/java/JavaRush/example/User.java
+++ Boby/src/main/java/JavaRush/example/User.java
@@ -1,4 +1,15 @@
 package JavaRush.example;
 
+import java.util.Map;
+
+class Sex{}
+
 public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Sex sex;
+
+    private static Map<Integer, User> allUsers;
+    private static int countId = 0;
 }
