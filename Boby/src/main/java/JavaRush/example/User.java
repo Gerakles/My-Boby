ab20f5605bb1372e73dfad21b4acd110f2cbbe98
Gerakles <31792347+Gerakles@users.noreply.github.com>
2018-08-13 22:27:27 +0300

    Update User.java

--- Boby/src/main/java/JavaRush/example/User.java
+++ Boby/src/main/java/JavaRush/example/User.java
@@ -3,8 +3,20 @@ package JavaRush.example;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.List;
+import java.util.ArrayList;
 
 class Sex {
+    private String male;
+    private String female;
+    
+    public static String Male () {
+        return "Male";
+    }
+    
+    public static String Female () {
+        return "Female";
+    }
 }
 
 public class User {
@@ -61,4 +73,77 @@ public class User {
                 ", sex=" + sex +
                 '}';
     }
+    
+    public static List<User> getAllUsers(){
+    return new ArrayList<>(allUsers.values());
+}
+
+public static List<User> getAllUsers(Sex sex){
+    List<User> listAllUsers = new ArrayList<>();
+    for (User user : allUsers.values()){
+        if (user.sex == sex){
+            listAllUsers.add(user);
+        }
+    }
+    return listAllUsers;
+}
+
+public static int getHowManyUsers(){
+    return allUsers.size();
+}
+
+public static int getHowManyUsers(Sex sex){
+    return getAllUsers(sex).size();
+}
+
+public static int getAllAgeUsers(){
+    int countAge = 0;
+    for (User user : allUsers.values()){
+        countAge += user.age;
+    }
+    return countAge;
+}
+
+public static int getAllAgeUsers(Sex sex){
+    int countAge = 0;
+    for (User user : getAllUsers(sex)){
+        countAge += user.age;
+    }
+    return countAge;
+}
+
+public static int getAverageAgeOfAllUsers(){
+    return getAllAgeUsers() / getHowManyUsers();
+}
+
+public static int getAverageAgeOfAllUsers(Sex sex){
+    return getAllAgeUsers(sex) / getHowManyUsers(sex);
+}
+
+public static void main(String[] args) {
+    new User("Евгений", 35, Sex.MALE);
+    new User("Марина", 34, Sex.FEMALE);
+    new User("Алина", 7, Sex.FEMALE);
+
+
+    System.out.println("Все пользователи:");
+    User.getAllUsers().forEach(System.out::println);
+    System.out.println("Все пользователи: MALE");
+    User.getAllUsers(Sex.MALE).forEach(System.out::println);
+    System.out.println("Все пользователи: FEMALE");
+    User.getAllUsers(Sex.FEMALE).forEach(System.out::println);
+    System.out.println("================================================");
+    System.out.println("       всех пользователей: " + User.getHowManyUsers());
+    System.out.println("  всех пользователей MALE: " + User.getHowManyUsers(Sex.MALE));
+    System.out.println("всех пользователей FEMALE: " + User.getHowManyUsers(Sex.FEMALE));
+    System.out.println("================================================");
+    System.out.println("       общий возраст всех пользователей: " + User.getAllAgeUsers());
+    System.out.println("  общий возраст всех пользователей MALE: " + User.getAllAgeUsers(Sex.MALE));
+    System.out.println("общий возраст всех пользователей FEMALE: " + User.getAllAgeUsers(Sex.FEMALE));
+    System.out.println("================================================");
+    System.out.println("       средний возраст всех пользователей: " + User.getAverageAgeOfAllUsers());
+    System.out.println("  средний возраст всех пользователей MALE: " + User.getAverageAgeOfAllUsers(Sex.MALE));
+    System.out.println("средний возраст всех пользователей FEMALE: " + User.getAverageAgeOfAllUsers(Sex.FEMALE));
+    System.out.println("================================================");
+}
 }
