67929f346176e3843dd051391fd0fa256b37df62
Gerakles <bogdania567@gmail.com>
2018-08-14 02:06:11 +0300

    Yakim 404                                        '+' 40

--- Boby/src/main/java/JavaRush/example/User.java
+++ Boby/src/main/java/JavaRush/example/User.java
@@ -7,15 +7,111 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class User {
-    public static enum Sex {
-        Male, Female
-    }
-    private static Map <Integer, User> allUsers;
     private static int countId = 0;
+    private static Map <Integer, User> allUsers = new HashMap <>();
     private int id;
     private String name;
     private int age;
     private Sex sex;
+
+    public User(String name, int age, Sex sex) {
+        if (name != null && !name.isEmpty() && age > 0 && sex != null) {
+
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+
+            if (!hasUser()) {
+                countId++;
+                this.id = countId;
+                allUsers.put( id, this );
+            }
+        }
+    }
+
+    public static List <User> getAllUsers() {
+        return new ArrayList <>( allUsers.values() );
+    }
+
+    public static List <User> getAllUsers(Sex sex) {
+        List <User> listAllUsers = new ArrayList <>();
+        for (User user : allUsers.values()) {
+            if (user.sex == sex) {
+                listAllUsers.add( user );
+            }
+        }
+        return listAllUsers;
+    }
+
+    public static int getHowManyUsers() {
+        return allUsers.size();
+    }
+
+    public static int getHowManyUsers(Sex sex) {
+        return getAllUsers( sex ).size();
+    }
+
+    public static int getAllAgeUsers() {
+        int countAge = 0;
+        for (User user : allUsers.values()) {
+            countAge += user.age;
+        }
+        return countAge;
+    }
+
+    public static int getAllAgeUsers(Sex sex) {
+        int countAge = 0;
+        for (User user : getAllUsers( sex )) {
+            countAge += user.age;
+        }
+        return countAge;
+    }
+
+    public static int getAverageAgeOfAllUsers() {
+        return getAllAgeUsers() / getHowManyUsers();
+    }
+
+    public static int getAverageAgeOfAllUsers(Sex sex) {
+        return getAllAgeUsers( sex ) / getHowManyUsers( sex );
+    }
+
+    public static void main(String[] args) {
+        System.out.println( "Все пользователи:" );
+        User.getAllUsers().forEach( System.out::println );
+        System.out.println( "Все пользователи: MALE" );
+        User.getAllUsers( Sex.Male ).forEach( System.out::println );
+        System.out.println( "Все пользователи: FEMALE" );
+        User.getAllUsers( Sex.Female ).forEach( System.out::println );
+        System.out.println( "================================================" );
+        System.out.println( "       всех пользователей: " + User.getHowManyUsers() );
+        System.out.println( "  всех пользователей MALE: " + User.getHowManyUsers( Sex.Male ) );
+        System.out.println( "всех пользователей FEMALE: " + User.getHowManyUsers( Sex.Female ) );
+        System.out.println( "================================================" );
+        System.out.println( "       общий возраст всех пользователей: " + User.getAllAgeUsers() );
+        System.out.println( "  общий возраст всех пользователей MALE: " + User.getAllAgeUsers( Sex.Male ) );
+        System.out.println( "общий возраст всех пользователей FEMALE: " + User.getAllAgeUsers( Sex.Female ) );
+        System.out.println( "================================================" );
+        System.out.println( "       средний возраст всех пользователей: " + User.getAverageAgeOfAllUsers() );
+        System.out.println( "  средний возраст всех пользователей MALE: " + User.getAverageAgeOfAllUsers( Sex.Male ) );
+        System.out.println( "средний возраст всех пользователей FEMALE: " + User.getAverageAgeOfAllUsers( Sex.Female ) );
+        System.out.println( "================================================" );
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -32,20 +128,6 @@ public class User {
         return Objects.hash( name, age, sex );
     }
 
-    public User(String name, int age, Sex sex) {
-        if (allUsers == null) {
-            allUsers = new HashMap <>(  );
-        }
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
-
-        if (!hasUser()) {
-            countId++;
-            this.id = countId;
-            allUsers.put( id, this );
-        }
-    }
     private boolean hasUser() {
         for (User user : allUsers.values()) {
             if (user.equals( this ) && user.hashCode() == this.hashCode())
@@ -64,76 +146,7 @@ public class User {
                 '}';
     }
 
-    public static List<User> getAllUsers(){
-        return new ArrayList<>(allUsers.values());
-    }
-
-    public static List<User> getAllUsers(Sex sex){
-        List<User> listAllUsers = new ArrayList<>();
-        for (User user : allUsers.values()){
-            if (user.sex == sex){
-                listAllUsers.add(user);
-            }
-        }
-        return listAllUsers;
-    }
-
-    public static int getHowManyUsers(){
-        return allUsers.size();
-    }
-
-    public static int getHowManyUsers(Sex sex){
-        return getAllUsers(sex).size();
-    }
-
-    public static int getAllAgeUsers(){
-        int countAge = 0;
-        for (User user : allUsers.values()){
-            countAge += user.age;
-        }
-        return countAge;
-    }
-
-    public static int getAllAgeUsers(Sex sex){
-        int countAge = 0;
-        for (User user : getAllUsers(sex)){
-            countAge += user.age;
-        }
-        return countAge;
-    }
-
-    public static int getAverageAgeOfAllUsers(){
-        return getAllAgeUsers() / getHowManyUsers();
-    }
-
-    public static int getAverageAgeOfAllUsers(Sex sex){
-        return getAllAgeUsers(sex) / getHowManyUsers(sex);
-    }
-
-    public static void main(String[] args) {
-        new User("Евгений", 35, Sex.Male);
-        new User("Марина", 34, Sex.Female);
-        new User("Алина", 7, Sex.Female);
-
-
-        System.out.println("Все пользователи:");
-        User.getAllUsers().forEach(System.out::println);
-        System.out.println("Все пользователи: MALE");
-        User.getAllUsers(Sex.Male).forEach(System.out::println);
-        System.out.println("Все пользователи: FEMALE");
-        User.getAllUsers(Sex.Female).forEach(System.out::println);
-        System.out.println("================================================");
-        System.out.println("       всех пользователей: " + User.getHowManyUsers());
-        System.out.println("  всех пользователей MALE: " + User.getHowManyUsers(Sex.Male));
-        System.out.println("всех пользователей FEMALE: " + User.getHowManyUsers(Sex.Female));
-        System.out.println("================================================");
-        System.out.println("       общий возраст всех пользователей: " + User.getAllAgeUsers());
-        System.out.println("  общий возраст всех пользователей MALE: " + User.getAllAgeUsers(Sex.Male));
-        System.out.println("общий возраст всех пользователей FEMALE: " + User.getAllAgeUsers(Sex.Female));
-        System.out.println("================================================");
-        System.out.println("       средний возраст всех пользователей: " + User.getAverageAgeOfAllUsers());
-        System.out.println("  средний возраст всех пользователей MALE: " + User.getAverageAgeOfAllUsers(Sex.Male));
-        System.out.println("средний возраст всех пользователей FEMALE: " + User.getAverageAgeOfAllUsers(Sex.Female));
-        System.out.println("================================================");
+    public enum Sex {
+        Male, Female
     }
 }
--- Boby/src/test/java/JavaRush/example/UserTest.java
+++ Boby/src/test/java/JavaRush/example/UserTest.java
@@ -1,40 +1,178 @@
 package JavaRush.example;
 
+import org.junit.Before;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.testng.Assert.*;
 
 public class UserTest {
 
     @Test
-    public void testGetAllUsers() {
+    public void getAllUsers() {
+        User user = new User( "Евгений", 35, User.Sex.Male );
+        User user1 = new User( "Марина", 34, User.Sex.Female );
+        User user2 = new User( "Алина", 7, User.Sex.Female );
+
+        List <User> expected = User.getAllUsers();
+
+        List <User> actual = new ArrayList <>();
+        actual.add( user );
+        actual.add( user1 );
+        actual.add( user2 );
+
+        Assert.assertEquals( expected, actual );
     }
 
     @Test
-    public void testGetAllUsers1() {
+    public void getAllUsers_NO_NULL() {
+        List <User> expected = User.getAllUsers();
+        Assert.assertNotNull( expected );
     }
 
     @Test
-    public void testGetHowManyUsers() {
+    public void getAllUsers_MALE() {
+        User user = new User( "Евгений", 35, User.Sex.Male );
+        User user1 = new User( "Марина", 34, User.Sex.Female );
+        User user2 = new User( "Алина", 7, User.Sex.Female );
+
+        List <User> expected = User.getAllUsers( User.Sex.Male );
+
+        List <User> actual = new ArrayList <>();
+        actual.add( user );
+
+        Assert.assertEquals( expected, actual );
     }
 
     @Test
-    public void testGetHowManyUsers1() {
+    public void getAllUsers_MALE_NO_NULL() {
+        List <User> expected = User.getAllUsers( User.Sex.Male );
+        Assert.assertNotNull( expected );
     }
 
     @Test
-    public void testGetAllAgeUsers() {
+    public void getAllUsers_FEMALE() {
+        User user = new User( "Евгений", 35, User.Sex.Male );
+        User user1 = new User( "Марина", 34, User.Sex.Female );
+        User user2 = new User( "Алина", 7, User.Sex.Female );
+
+        List <User> expected = User.getAllUsers( User.Sex.Female );
+
+        List <User> actual = new ArrayList <>();
+        actual.add( user1 );
+        actual.add( user2 );
+
+        Assert.assertEquals( expected, actual );
     }
 
     @Test
-    public void testGetAllAgeUsers1() {
+    public void getAllUsers_FEMALE_NO_NULL() {
+        List <User> expected = User.getAllUsers( User.Sex.Female );
+        Assert.assertNotNull( expected );
     }
 
     @Test
-    public void testGetAverageAgeOfAllUsers() {
+    public void getHowManyUsers() {
+        User user = new User( "Евгений", 35, User.Sex.Male );
+        User user1 = new User( "Марина", 34, User.Sex.Female );
+        User user2 = new User( "Алина", 7, User.Sex.Female );
+
+        int expected = User.getHowManyUsers();
+        int actual = 3;
+
+        Assert.assertEquals( expected, actual );
     }
 
     @Test
-    public void testGetAverageAgeOfAllUsers1() {
+    public void getHowManyUsers_MALE() {
+        User user = new User( "Евгений", 35, User.Sex.Male );
+        User user1 = new User( "Марина", 34, User.Sex.Female );
+        User user2 = new User( "Алина", 7, User.Sex.Female );
+
+        int expected = User.getHowManyUsers( User.Sex.Male );
+        int actual = 1;
+
+        Assert.assertEquals( expected, actual );
+    }
+
+    @Test
+    public void getHowManyUsers_FEMALE() {
+        User user = new User( "Евгений", 35, User.Sex.Male );
+        User user1 = new User( "Марина", 34, User.Sex.Female );
+        User user2 = new User( "Алина", 7, User.Sex.Female );
+
+        int expected = User.getHowManyUsers( User.Sex.Female );
+        int actual = 2;
+
+        Assert.assertEquals( expected, actual );
+    }
+
+    @Test
+    public void getAllAgeUsers() {
+        User user = new User( "Евгений", 35, User.Sex.Male );
+        User user1 = new User( "Марина", 34, User.Sex.Female );
+        User user2 = new User( "Алина", 7, User.Sex.Female );
+
+        int expected = User.getAllAgeUsers();
+
+        int actual = 35 + 34 + 7;
+
+        Assert.assertEquals( expected, actual );
+    }
+
+    @Test
+    public void getAllAgeUsers_MALE() {
+        User user = new User( "Евгений", 35, User.Sex.Male );
+        User user1 = new User( "Марина", 34, User.Sex.Female );
+        User user2 = new User( "Алина", 7, User.Sex.Female );
+
+        int expected = User.getAllAgeUsers( User.Sex.Male );
+
+        int actual = 35;
+
+        Assert.assertEquals( expected, actual );
+    }
+
+    @Test
+    public void getAllAgeUsers_FEMALE() {
+        User user = new User( "Евгений", 35, User.Sex.Male );
+        User user1 = new User( "Марина", 34, User.Sex.Female );
+        User user2 = new User( "Алина", 7, User.Sex.Female );
+
+        int expected = User.getAllAgeUsers( User.Sex.Female );
+
+        int actual = 34 + 7;
+
+        Assert.assertEquals( expected, actual );
+    }
+
+    @Test
+    public void newUser_EMPTY_NAME() {
+        for (User user : User.getAllUsers()){
+            if (user.getName() != null && user.getName().isEmpty()) {
+                Assert.fail("Попытка создания пользователя с пустым именем");
+            }
+        }
+    }
+
+    @Test
+    public void newUser_AGE_ZERO() {
+        for (User user : User.getAllUsers()) {
+            if (user.getAge() <= 0) {
+                Assert.fail("Попытка создания пользователя c не допустимым возрастом");
+            }
+        }
+    }
+
+    @Test
+    public void newUser_SEX_NO_NULL() {
+        for (User user : User.getAllUsers()) {
+            if (user.getSex() == null) {
+                Assert.fail("Попытка создания пользователя с указанием пола = null");
+            }
+        }
     }
 }
