4075341683321ba071a41af84948c56009749889
Gerakles <bogdania567@gmail.com>
2018-08-14 00:41:14 +0300

    Yakim 404                                        '+' 40

---
+++ Boby/src/test/java/JavaRush/example/UserTest.java
@@ -0,0 +1,40 @@
+package JavaRush.example;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void testGetAllUsers() {
+    }
+
+    @Test
+    public void testGetAllUsers1() {
+    }
+
+    @Test
+    public void testGetHowManyUsers() {
+    }
+
+    @Test
+    public void testGetHowManyUsers1() {
+    }
+
+    @Test
+    public void testGetAllAgeUsers() {
+    }
+
+    @Test
+    public void testGetAllAgeUsers1() {
+    }
+
+    @Test
+    public void testGetAverageAgeOfAllUsers() {
+    }
+
+    @Test
+    public void testGetAverageAgeOfAllUsers1() {
+    }
+}
