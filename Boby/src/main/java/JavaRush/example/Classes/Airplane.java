770192eba8490e345309e64451d8a18f7612852d
Gerakles <bogdania567@gmail.com>
2018-08-15 04:45:27 +0300

    Yakim 403                                       '+' 39

--- Boby/src/main/java/JavaRush/example/Classes/Airplane.java
+++ Boby/src/main/java/JavaRush/example/Classes/Airplane.java
@@ -1,5 +1,21 @@
 package JavaRush.example.Classes;
 
 public class Airplane {
-    
+    private String name, id, flight;
+    private Wing leftWing = new Wing("Red", "y3"), rightWing = new Wing("Blue", "y3");
+
+    public Airplane(String name, String id, String flight) {
+        this.name = name;
+        this.id = id;
+        this.flight = flight;
+    }
+
+    private class Wing {
+        private String color, model;
+
+        public Wing(String color, String model) {
+            this.color = color;
+            this.model = model;
+        }
+    }
 }
