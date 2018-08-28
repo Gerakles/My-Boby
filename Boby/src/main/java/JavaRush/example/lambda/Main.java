e002d4a2c44e9b16d8edcd250aaea1c11921fc06
Gerakles <bogdania567@gmail.com>
2018-08-28 14:36:58 +0300

    Yakim 392                        '+' 24

---
+++ Boby/src/main/java/JavaRush/example/lambda/Main.java
@@ -0,0 +1,10 @@
+package JavaRush.example.lambda;
+
+public class Main {
+    public static void main(String[] args) {
+        Runnable runnable = 
+    }
+    private static void staticMethod() {
+        System.out.println("Method static");
+    }
+}
