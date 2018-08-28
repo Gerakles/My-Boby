0864a4bbb37fd3ee2bc53e80eb488eeb44f1561b
Gerakles <bogdania567@gmail.com>
2018-08-28 14:32:53 +0300

    Yakim 392                        '+' 24

--- Boby/src/main/java/JavaRush/example/lambda.java
+++ Boby/src/main/java/JavaRush/example/lambda.java
@@ -1,4 +1,10 @@
 package JavaRush.example;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class lambda {
+    public static void main(String[] args) {
+        final AtomicInteger counter = new AtomicInteger( 0 );
+        Runnable r =()-> counter.incrementAndGet();
+    }
 }
