c569dfeb87c02d2b712079c80946037a0bd5b0d6
Gerakles <bogdania567@gmail.com>
2018-08-28 14:42:26 +0300

    Yakim 392                        '+' 24

--- Boby/src/main/java/JavaRush/example/lambda/Main.java
+++ Boby/src/main/java/JavaRush/example/lambda/Main.java
@@ -2,8 +2,14 @@ package JavaRush.example.lambda;
 
 public class Main {
     public static void main(String[] args) {
-        Runnable runnable =() -> staticMethod();
-        new Thread( runnable).start();
+//        Runnable runnable =() -> staticMethod();
+//        new Thread( runnable).start();
+        System.out.println("run program");
+        System.out.println("after lambda");
+        Runnable runnable = () -> System.out.println("I am lambda");
+        System.out.println("post lambda");
+        System.out.println("move lambda in thread");
+        new Thread( runnable ).start();
     }
     private static void staticMethod() {
         System.out.println("Method static");
