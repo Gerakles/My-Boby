9a8853e7d9974e6aa728ce0d3dd0147aac78c5d8
Gerakles <bogdania567@gmail.com>
2018-08-28 14:45:40 +0300

    Yakim 392                        '+' 24

--- Boby/src/main/java/JavaRush/example/lambda/Main.java
+++ Boby/src/main/java/JavaRush/example/lambda/Main.java
@@ -4,14 +4,15 @@ public class Main {
     public static void main(String[] args) {
 //        Runnable runnable =() -> staticMethod();
 //        new Thread( runnable).start();
-        System.out.println("run program");
-        System.out.println("after lambda");
-        Runnable runnable = () -> System.out.println("I am lambda");
-        System.out.println("post lambda");
-        System.out.println("move lambda in thread");
+        System.out.println( "run program" );
+        System.out.println( "after lambda" );
+        Runnable runnable = () -> System.out.println( "I am lambda" );
+        System.out.println( "post lambda" );
+        System.out.println( "move lambda in thread" );
         new Thread( runnable ).start();
     }
+
     private static void staticMethod() {
-        System.out.println("Method static");
+        System.out.println( "Method static" );
     }
 }
--- Boby/src/main/java/JavaRush/example/lambda/lambda.java
+++ Boby/src/main/java/JavaRush/example/lambda/lambda.java
@@ -5,6 +5,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class lambda {
     public static void main(String[] args) {
         final AtomicInteger counter = new AtomicInteger( 0 );
-        Runnable r =()-> counter.incrementAndGet();
+        Runnable r = () -> counter.incrementAndGet();
     }
 }
