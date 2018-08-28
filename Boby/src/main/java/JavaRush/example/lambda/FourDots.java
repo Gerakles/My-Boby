796f1016a10decccf695a7b6c07b836c73e6f7c1
Gerakles <bogdania567@gmail.com>
2018-08-29 00:14:45 +0300

    Yakim 391                       '+' 23

--- Boby/src/main/java/JavaRush/example/lambda/FourDots.java
+++ Boby/src/main/java/JavaRush/example/lambda/FourDots.java
@@ -10,6 +10,9 @@ public class FourDots {
         strings.add( " or " );
         strings.add( "not to be" );
 
-        strings.forEach( System.out::print );
+//        strings.forEach( System.out::print );
+        strings.forEach( Main::staticMethod );
+    }
+    private void staticMethod(String s) {
     }
 }
