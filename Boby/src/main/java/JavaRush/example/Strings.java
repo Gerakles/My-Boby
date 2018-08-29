603dcfe82b7fc824bede5c70bdc515ba195ab6a7
Gerakles <bogdania567@gmail.com>
2018-08-29 16:45:08 +0300

    Yakim 391                       '+' 23

--- Boby/src/main/java/JavaRush/example/Strings.java
+++ Boby/src/main/java/JavaRush/example/Strings.java
@@ -1,4 +1,9 @@
 package JavaRush.example;
 
 public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String text = "X = "+ x;
+        System.out.println(text);
+    }
 }
