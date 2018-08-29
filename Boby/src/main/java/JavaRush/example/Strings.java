e5dafeba0247e141c23db145be255236c1485f83
Gerakles <bogdania567@gmail.com>
2018-08-29 23:45:23 +0300

    Yakim 391                       '+' 23

--- Boby/src/main/java/JavaRush/example/Strings.java
+++ Boby/src/main/java/JavaRush/example/Strings.java
@@ -3,8 +3,8 @@ package JavaRush.example;
 public class Strings {
     public static void main(String[] args) {
         int x = 5;
-        String text = "X = "+ x;
-        System.out.println(text);
+        String text = "X = " + x;
+        System.out.println( text );
         String test = "1000";
         int i = Integer.parseInt( test.trim() );
         System.out.println( i );
