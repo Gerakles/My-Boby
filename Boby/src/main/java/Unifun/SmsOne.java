028325ea7294aaabcbbaaa199aa8b42717de402d
imac <iachimciucbogdan@gmail.com>
2018-09-05 01:01:12 +0300

    SmsC

--- Boby/src/main/java/Unifun/SmsOne.java
+++ Boby/src/main/java/Unifun/SmsOne.java
@@ -1,4 +1,16 @@
 package Unifun;
 
+import Unifun.Library.Smsc;
+
 public class SmsOne {
+
+    public static void main(String[] args) {
+
+        Smsc smsc = new Smsc();
+
+        String [] ret = smsc.send_sms("+37379839605", "Hello", 1, "", "", 0, "", "");
+        String balance = smsc.get_balance();
+
+        System.out.println(balance);
+    }
 }
